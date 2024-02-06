15b5f2dd7084a28dd8a0008a269331aa6282ba14
Emilton Nhampossa <123878288+emiltonnhampossa@users.noreply.github.com>
2024-02-07 03:31:00 +1400

    chore:instalando tailwind css e eslint

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import logo from './assets/Logo_nlw_escuro.svg'
+
+export function App() {
+  return (
+    <div>
+      <img src={logo} />
+      <input type="text" placeholder="Busque em suas notas" />
+    </div>
+  )
+}
