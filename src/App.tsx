2c425c5031b2f508bb6c51d584b232d3ca579281
Emilton Nhampossa <123878288+emiltonnhampossa@users.noreply.github.com>
2024-02-09 10:29:46 +1400

    Chore:Adicionar a funcao de tranformacao de voz em texto e local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,63 @@
+import { ChangeEvent, useState } from 'react'
 import logo from './assets/Logo_nlw_escuro.svg'
 import NewNoteCard from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+interface Note {
+  id: string
+  date: Date
+  content: string
+}
+
 export function App() {
+  const [search, setSearch] = useState('')
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+
+    return []
+  })
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    const notesArray = [newNote, ...notes]
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter((note) => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filterNotes =
+    search !== ''
+      ? notes.filter((notes) =>
+          notes.content.toLowerCase().includes(search.toLowerCase()),
+        )
+      : notes
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5 ">
       <img src={logo} alt="nlw-expert" />
 
       <form className="w-full">
@@ -12,20 +65,19 @@ export function App() {
           type="text"
           placeholder="Busque em suas notas"
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          onChange={handleSearch}
         />
       </form>
 
       <div className="h-px bg bg-slate-700" />
 
-      <div className="grid grid-cols-3 gap-6 auto-rows-[250px] ">
-        <NewNoteCard />
-        <NoteCard
-          note={{
-            date: new Date(),
-            content:
-              'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, doloribus ducimus quis alias nobis, dolores soluta corrupti non perspiciatis hic cum corporis odio error rerum numquam! Harum perferendis ea iste!',
-          }}
-        />
+      <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px] ">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {filterNotes.map((note) => {
+          return (
+            <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
+          )
+        })}
       </div>
     </div>
   )
